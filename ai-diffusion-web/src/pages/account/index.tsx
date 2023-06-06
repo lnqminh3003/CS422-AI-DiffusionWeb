3201b024efba4cba769099345a5ee844e2f0add0
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2023-06-06 15:57:11 +0700

    update

--- ai-diffusion-web/src/pages/account/index.tsx
+++ ai-diffusion-web/src/pages/account/index.tsx
@@ -234,7 +234,7 @@ export default function Collection() {
                       type="button"
                       className="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
-                      Let's go
+                      Thank you
                     </button>
                   </div>
                 </div>
--- ai-diffusion-web/src/pages/collection/index.tsx
+++ ai-diffusion-web/src/pages/collection/index.tsx
@@ -96,12 +96,13 @@ export default function Collection() {
         </div>
         <div id="all" className=" ml-8 grid grid-cols-4 gap-4 content-center">
           {pn.map((a: any) => (
-                <img
-                key={a.image}
-                  src={a.image}
-                  alt=""
-                  className=" w-60 h-60 object-cover rounded-3xl items-center"
-                />
+                <button key={a.image} onClick={()=>{console.log("aaaa")}}>
+                  <img
+                    src={a.image}
+                    alt=""
+                    className=" w-60 h-60 object-cover rounded-3xl items-center"
+                  />
+                </button>
           ))}
         </div>
       </div>
